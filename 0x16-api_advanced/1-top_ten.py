e50aa50dd3dff96540436fabf114f69fb465c65e
GKiarie <igitaukiarie@gmail.com>
2023-05-09 21:42:46 +0300

    hotfix task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ import requests
 
 def top_ten(subreddit):
     """
-    function that prints the top ten hot posts
+    ftn that prints the top ten posts
     listed for a given subreddit
     """
     headers = {
@@ -15,8 +15,10 @@ def top_ten(subreddit):
                     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61\
                     Safari/537.36 Brave/94.1.23.1 Chrome/94.0.4606.61'
                     }
-    response = requests.get(f"https://www.reddit.com/r/{subreddit}/hot.json?\
-                            limit=10", headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, headers=headers,
+                            allow_redirects=False)
+    print(response)
     if response.status_code == 404:
         print(None)
         return
