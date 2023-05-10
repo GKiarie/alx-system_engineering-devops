8db95b7f1c2eef2e0d31903cb011bef565f8ada6
GKiarie <igitaukiarie@gmail.com>
2023-05-10 21:45:34 +0300

    update strace tsk fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # Using puppet to fix problem of Apache
 # returning a 500 error
 
-exec { 'Fixing_apache2':
+exec { 'fixing_apache':
     command => "sudo sed -i 's/phpp/php/g' '/var/www/html/wp-settings.php'"
-    path => '/usr/local/bin/:/bin/'
+    path    => ['/bin', '/usr/bin']
 }
